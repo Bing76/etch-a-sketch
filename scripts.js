5b05c16fdb9b23bba955cd4c92d9b44cea984f14
Alex Yang <jyan8933@uni.sydney.edu.au>
2025-03-04 21:27:27 +1100

    Add logic for keeping grid of squares at constant size for changing square no.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,43 @@
 
-
-// Create 16 x 16 grid of divs
 const container = document.querySelector(".container");
+const dimensionsBtn = document.querySelector(".dimensions");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
-    
-    for (let j = 0; j < 16; j++) {
-        const col = document.createElement("div");
-        col.setAttribute("class", "col");
+function setupGrid(dimension) {
+    const width = 960 / dimension;
+    const height = 960 / dimension;
 
-        // Trail effect
-        col.addEventListener("mouseover", () => {
-            col.style.backgroundColor = "black";
-        })
 
-        row.appendChild(col);
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        
+        for (let j = 0; j < dimension; j++) {
+            const col = document.createElement("div");
+            col.setAttribute("class", "col");
+            col.style.width = `${width}px`;
+            col.style.height = `${height}px`;
+
+            // Trail effect
+            col.addEventListener("mouseover", () => {
+                col.style.backgroundColor = "black";
+            })
+
+            row.appendChild(col);
+        }
+
+        container.appendChild(row);
+    }
+}
+
+dimensionsBtn.addEventListener("click", () => {
+    let dimension = 0;
+    while (isNaN(dimension) || dimension < 1 || dimension > 100) {
+        dimension = prompt("Please enter new number of squares per side. (No more than 100)");
     }
 
-    container.appendChild(row);
-}
+    container.replaceChildren();
+    setupGrid(dimension);
+})
+
+setupGrid(100);
+
