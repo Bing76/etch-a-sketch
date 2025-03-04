2364a86799e9f49a5c1ab0222d39614450f01ef7
Alex Yang <jyan8933@uni.sydney.edu.au>
2025-03-04 12:45:40 +1100

    Create 16x16 grid of divs with layout

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+
+// Create 16 x 16 grid of divs
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    
+    for (let j = 0; j < 16; j++) {
+        const col = document.createElement("div");
+        col.setAttribute("class", "col");
+        row.appendChild(col);
+    }
+
+    container.appendChild(row);
+}
